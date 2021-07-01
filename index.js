e5b1ff7d382b28af9a35fedff6f2fc645bb46787
Daniel Hoffmann Bernardes <daniel.hoffmann.bernardes@gmail.com>
2021-07-01 10:32:45 +0200

    playground

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const typeDefs = require("./schema");
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: {},
+});
 
 // The `listen` method launches a web server.
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
